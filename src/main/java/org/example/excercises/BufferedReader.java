08c688625726a5f211ec6a4f3c18b25612788c71
ksx <kd.stajkowski@gmail.com>
2022-06-27 16:59:12 +0200

    BufferedReader

---
+++ src/main/java/org/example/excercises/BufferedReader.java
@@ -0,0 +1,19 @@
+package org.example.excercises;
+
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BufferedReader {
+    public static void main(String[] args) throws IOException {
+
+        java.io.BufferedReader reader = new java.io.BufferedReader(
+                new InputStreamReader(System.in)
+        );
+
+        System.out.println("Podaj imie");
+        String name = reader.readLine();
+        System.out.println("Twoje imie to: " + name);
+
+    }
+}
