dc33a198a51144a311889375a3169101ebdbe869
ksx <kd.stajkowski@gmail.com>
2022-06-27 16:22:26 +0200

    variable wrapper

---
+++ src/main/java/org/example/data/types/J06Wrapper_classes.java
@@ -0,0 +1,11 @@
+package org.example.data.types;
+
+public class J06Wrapper_classes {
+    public static void main(String[] args) {
+        int a = 123;
+        Integer number = Integer.valueOf(a);
+        System.out.println(number.toString());
+        System.out.println(number.floatValue());
+        System.out.println(Integer.toHexString(a));
+    }
+}
