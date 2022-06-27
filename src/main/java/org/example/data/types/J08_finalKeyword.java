6a641d15aa02008cb8dd156f0785d59019b5930f
ksx <kd.stajkowski@gmail.com>
2022-06-27 16:30:23 +0200

    final variable

---
+++ src/main/java/org/example/data/types/J08_finalKeyword.java
@@ -0,0 +1,13 @@
+package org.example.data.types;
+
+public class J08_finalKeyword {
+    public static void main(String args[]){
+
+        int number = 10;
+        number = 32;
+
+        final int num2 = 100;
+        // num2 = 200;
+
+    }
+}
