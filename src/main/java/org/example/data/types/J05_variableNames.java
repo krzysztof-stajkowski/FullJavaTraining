35ea1312f4ca8e8a012a7d32f6d92f270418a2db
ksx <kd.stajkowski@gmail.com>
2022-06-27 16:22:14 +0200

    variable names

---
+++ src/main/java/org/example/data/types/J05_variableNames.java
@@ -0,0 +1,11 @@
+package org.example.data.types;
+
+public class J05_variableNames {
+    public static void main(String[] args) {
+        int _int1 = 10;
+        int currentHour = 12; //most recommended
+        float moneyInThePocket = 23.12f; //most recommended
+        double $speed = 100.10d;
+
+    }
+}
