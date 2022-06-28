a459d5454dcb948f63f10255a2560536b3d1ce3d
ksx <kd.stajkowski@gmail.com>
2022-06-28 19:02:44 +0200

    Operators - relational_operators - operatory porównania

---
+++ src/main/java/org/example/data/operators/op3_relational_operators.java
@@ -0,0 +1,21 @@
+package org.example.data.operators;
+
+public class op3_relational_operators {
+    public static void main(String[] args) {
+
+        boolean b1 = 5 == 3;
+        boolean b2 = 7 == 7;
+        boolean b3 = 7 != 7;
+        boolean b4 = 7 != 5;
+        boolean b5 = 10 > 4;
+        boolean b6 = 10 < 8;
+        boolean b7 = 8 >= 6;
+        boolean b8 = 8 >= 8;
+        boolean b9 = 8 > 8;
+        boolean b10 = 10<=12;
+        boolean b11 = 10<=10;
+
+        System.out.println(b1);
+
+    }
+}
