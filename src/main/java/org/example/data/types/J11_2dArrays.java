220ae9a88a7d61aefcf71ad992e8d28492bd1470
ksx <kd.stajkowski@gmail.com>
2022-06-28 13:21:40 +0200

    2dArrays

--- src/main/java/org/example/data/types/J11_2dArrays.java
+++ src/main/java/org/example/data/types/J11_2dArrays.java
@@ -3,5 +3,20 @@ package org.example.data.types;
 public class J11_2dArrays {
     public static void main(String args[]) {
 
+        String strArr[][] = {
+                {"1", "2", "3"},
+                {"4", "5", "6"}
+        };
+
+        System.out.println(strArr[0][1]); //2
+        System.out.println(strArr[1][2]); //6
+
+        int numArr[][] = new int[3][4];
+        numArr[1][2] = 100;
+
+        int numRows = 10;
+        int numCols = 15;
+        int arr[][] = new int [numRows][numCols];
+
     }
 }
