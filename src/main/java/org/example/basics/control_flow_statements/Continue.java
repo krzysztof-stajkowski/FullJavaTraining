74d1d6cd07e656b9e54498db8e43019644d06833
ksx <kd.stajkowski@gmail.com>
2022-07-05 18:42:57 +0200

    Continue - instruction to exit a part of loop

---
+++ src/main/java/org/example/basics/control_flow_statements/Continue.java
@@ -0,0 +1,17 @@
+package org.example.basics.control_flow_statements;
+
+/*
+
+Dzięki tej instrukcji continue wychodzimy z pętli ale jest ona dalej wykonywana np. możemy pominąć jakieś elementy na podstawie warunków IF
+
+ */
+
+public class Continue {
+    public static void main(String[] args) {
+        for(int a = 0; a < 10; a++) {
+            if(a == 2 || a == 5 || a == 9) continue;
+            System.out.println(a);
+        }
+
+    }
+}
