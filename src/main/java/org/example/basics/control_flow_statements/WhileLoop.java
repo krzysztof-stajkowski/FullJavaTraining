9e48f140a13ebd0527180c0d3dbdaf197d42fe47
ksx <kd.stajkowski@gmail.com>
2022-07-05 17:53:54 +0200

    While loop basics

---
+++ src/main/java/org/example/basics/control_flow_statements/WhileLoop.java
@@ -0,0 +1,21 @@
+package org.example.basics.control_flow_statements;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+
+        int i = 5;
+
+        while (i > 0) {
+            System.out.println(i);
+            i--;
+        }
+
+        i = 0;
+
+        while (i < 6) {
+            System.out.println("i: "+ i);
+            i++;
+        }
+
+    }
+}
