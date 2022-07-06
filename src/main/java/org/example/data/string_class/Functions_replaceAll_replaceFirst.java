b1d6befb2de8625d813a244bc9f3c177b231c6fb
ksx <kd.stajkowski@gmail.com>
2022-07-06 14:42:57 +0200

    Strings replaceAll replaceFirst

---
+++ src/main/java/org/example/data/string_class/Functions_replaceAll_replaceFirst.java
@@ -0,0 +1,17 @@
+package org.example.data.string_class;
+
+public class Functions_replaceAll_replaceFirst {
+    public static void main(String args[]) {
+
+
+        //replaceAll podmienia wszystkie wystąpienia
+        String str = "Jan Kowalski Kasia Kowalska";
+        String str1 = str.replaceAll("Kowalska", "Zawadzka");
+        System.out.println(str1); // Jan Kowalski Kasia Zawadzka
+
+        //replaceFirst tylko pierwsze znalezione
+        String str2 = str1.replaceFirst(" ", " Junior ");
+        System.out.println(str2); // Jan Junior Kowalski Kasia Zawadzka
+
+    }
+}
