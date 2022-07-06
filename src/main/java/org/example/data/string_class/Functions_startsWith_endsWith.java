83c08c16528d2911189942481154d1c77a3b2b3d
ksx <kd.stajkowski@gmail.com>
2022-07-06 14:39:37 +0200

    Strings startsWith endsWith

---
+++ src/main/java/org/example/data/string_class/Functions_startsWith_endsWith.java
@@ -0,0 +1,13 @@
+package org.example.data.string_class;
+
+public class Functions_startsWith_endsWith {
+    public static void main(String[] args) {
+        String str = "Java program";
+
+        if(str.startsWith("PHP")) System.out.println("Starts with PHP");
+
+        if(str.startsWith("Java")) System.out.println("Starts with Java");
+
+        if(str.endsWith("program")) System.out.println("Ends with program");
+    }
+}
