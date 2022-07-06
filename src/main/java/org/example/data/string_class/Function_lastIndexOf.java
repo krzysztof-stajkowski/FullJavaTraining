77764ddfe06f8a39c118668ddfe0e1393ed83b1c
ksx <kd.stajkowski@gmail.com>
2022-07-06 14:59:42 +0200

    Strings indexOf lastIndexOf with substring

---
+++ src/main/java/org/example/data/string_class/Function_lastIndexOf.java
@@ -0,0 +1,19 @@
+package org.example.data.string_class;
+
+public class Function_lastIndexOf {
+    public static void main(String args[]) {
+     //lastIndexOf szuka numer od końca Stringa, ale poda index taki jakby szukał od początku czyli rzeczywisty index a nie pozycję od końca
+
+        String str = "Raz dwa trzy cztery. Raz dwa.";
+
+        int index = str.lastIndexOf("Raz");
+        System.out.println("Ostatnie wystąpienie 'Raz': " + index);
+
+        String fragment = str.substring(index);
+        System.out.println("Fragment: " + fragment);
+
+        index = str.indexOf("Raz");
+        System.out.println("Pierwsze wystąpienie 'Raz': " + index);
+
+    }
+}
