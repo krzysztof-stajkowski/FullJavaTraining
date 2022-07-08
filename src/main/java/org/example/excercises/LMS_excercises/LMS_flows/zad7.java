930de1a875c67aca65275cbe64942af56a614e27
ksx <kd.stajkowski@gmail.com>
2022-07-08 16:58:04 +0200

    Training excercises done

---
+++ src/main/java/org/example/excercises/LMS_excercises/LMS_flows/zad7.java
@@ -0,0 +1,21 @@
+package org.example.excercises.LMS_excercises.LMS_flows;
+/*
+Zadanie 7
+
+    W pliku Main07.java stwórz dwie pętle i wypisz wartości ich liczników
+    w każdej iteracji dla i < 3 oraz j < 3.
+    Wykorzystaj: System.out.println("i = " + i + " j = " + j);
+    Zadanie rozwiąż używając dwóch pętli for.
+ */
+
+public class zad7 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 3 ; i++) {
+
+            for (int j = 0; j < 3 ; j++) {
+                System.out.println("i = " + i + " j = " + j);
+            }
+        }
+    }
+}
