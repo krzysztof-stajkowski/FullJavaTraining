7ec83a6effdc69115e176bc1676409a3160c1b0e
ksx <kd.stajkowski@gmail.com>
2022-07-10 14:19:39 +0200

    String tests/tries/experiments

---
+++ src/main/java/org/example/data/string_class/StringVariusTests.java
@@ -0,0 +1,23 @@
+package org.example.data.string_class;
+
+public class StringVariusTests {
+    public static void main(String[] args) {
+        String str = "Hello";
+        String str2 = new String(" World");
+        StringBuilder StrB = new StringBuilder("Hello ");
+        StrB.append(5);
+        StrB.append(" World");
+
+        System.out.println(str + str2);
+
+        System.out.println("string builder: -------------");
+
+        System.out.println(StrB);
+        System.out.println(StrB.lastIndexOf("l"));
+        StrB.delete(11,13);
+        System.out.println(StrB);
+        StrB.substring(5);
+        System.out.println(StrB.substring(5).trim().toUpperCase());
+        System.out.println(StrB.substring(5).trim().toLowerCase());
+    }
+}
