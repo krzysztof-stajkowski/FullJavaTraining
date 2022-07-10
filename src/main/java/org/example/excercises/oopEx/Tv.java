0d3927173cced60aa6421f6528da525cfe13f5db
ksx <kd.stajkowski@gmail.com>
2022-07-10 17:05:38 +0200

    oop basic example excercise

---
+++ src/main/java/org/example/excercises/oopEx/Tv.java
@@ -0,0 +1,33 @@
+package org.example.excercises.oopEx;
+
+public class Tv {
+
+    String producent;
+    String model;
+
+    int rok_produkcji;
+    public void printBasicData() {
+        System.out.println(
+                "Producent: " + this.producent + " \n" +
+                        "Model: " + this.model + " \n" +
+                        "Rok produkcji: " + this.rok_produkcji
+        );
+    }
+}
+
+class TvExample {
+
+    public static void main(String[] args) {
+
+        Tv telewizor1 = new Tv();
+        telewizor1.producent = "Sony";
+        telewizor1.model = "Bravia";
+        telewizor1.rok_produkcji = 2020;
+
+        telewizor1.printBasicData();
+
+
+    }
+
+}
+
