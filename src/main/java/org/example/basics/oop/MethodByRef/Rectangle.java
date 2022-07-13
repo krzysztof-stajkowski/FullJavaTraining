b1a6751713e37cd59749f7fbe60265e6d7410463
ksx <kd.stajkowski@gmail.com>
2022-07-13 17:09:18 +0200

    MethodByref calculations

--- src/main/java/org/example/basics/oop/MethodByRef/Rectangle.java
+++ src/main/java/org/example/basics/oop/MethodByRef/Rectangle.java
@@ -9,7 +9,7 @@ public class Rectangle {
 
     float circuit;
 
-    //kontsruktor robię ale tylko z dwóch boków bo to one służą do obliczenia powieszchni
+    //konstruktor robię ale tylko z dwóch boków bo to one służą do obliczenia powieszchni
     public Rectangle(float s1, float s2) {
         this.side1 = s1;
         this.side2 = s2;
