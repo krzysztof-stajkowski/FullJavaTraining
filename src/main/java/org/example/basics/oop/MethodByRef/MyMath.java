bbca20b09338ba48c8f5e99cc85d3f0135040d64
ksx <kd.stajkowski@gmail.com>
2022-07-13 17:09:03 +0200

    MethodByref calculations

--- src/main/java/org/example/basics/oop/MethodByRef/MyMath.java
+++ src/main/java/org/example/basics/oop/MethodByRef/MyMath.java
@@ -6,11 +6,9 @@ public class MyMath {
     //tworzę sobie metodę która z obiektu obliczy trzecie pole czyli surfaceArea
     public void calcRectangleSurface(Rectangle rectangle) {
         rectangle.surfaceArea = rectangle.side1 * rectangle.side2;
-
     }
 
     public void circuit(Rectangle rectangle) {
         rectangle.circuit = (rectangle.side1 + rectangle.side2) * 2;
     }
-
 }
