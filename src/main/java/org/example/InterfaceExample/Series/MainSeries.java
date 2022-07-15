a1744a12443dadc26f893dd5f743daf4b1ab7b2e
ksx <kd.stajkowski@gmail.com>
2022-07-15 19:55:12 +0200

    Interfaces - method in object

--- src/main/java/org/example/InterfaceExample/Series/MainSeries.java
+++ src/main/java/org/example/InterfaceExample/Series/MainSeries.java
@@ -9,9 +9,21 @@ public class MainSeries {
 
         //teraz można zrobić pętlę która wywoła 5 razy metodę z klasy byTwos implementującej metody z interfejsu
         for (int i = 0; i < 5; i++) {
-            //System.out.println(byTwos.getNext());
-            System.out.println(byThrees.getNext());
+            System.out.println("byTwo standard: " + byTwos.getNext());
+            System.out.println("byThree standard: " + byThrees.getNext());
         }
+
+        //można sworzyć obiekt typu interfejs który będzie można przypisać do metody
+        Series ob;
+
+        for (int i = 0; i < 5; i++) {
+            ob = byTwos;
+            System.out.println("Obiekt byTwo: " + ob.getNext());
+
+            ob = byThrees;
+            System.out.println("Obiekt byThree: " + ob.getNext());
+        }
+
 
     }
 }
