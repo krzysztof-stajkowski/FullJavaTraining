7efa8d3f420f60175268c9044cea44644487d539
ksx <kd.stajkowski@gmail.com>
2022-07-15 20:29:21 +0200

    Interfaces - extend interfaces

---
+++ src/main/java/org/example/InterfaceExample/Series/SeriesSub.java
@@ -0,0 +1,9 @@
+package org.example.InterfaceExample.Series;
+
+//interfejsy mogą być dziedziczone
+//oznacza to że SeriesSub będzie miało metody Series
+public interface SeriesSub extends Series  {
+
+    int getPrevSecond();
+
+}
