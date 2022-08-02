003a59d98f513cdd2f881a4404c72a76b481118e
ksx <kd.stajkowski@gmail.com>
2022-08-02 11:50:08 +0200

    Singleton

---
+++ src/main/java/org/example/wzorceProjektowe/Singleton.java
@@ -0,0 +1,31 @@
+package org.example.wzorceProjektowe;
+
+public class Singleton { //jedna instancja obiektu nieważne ile razy bedzie wywołana metoda np. raz pobieramy plik 1GB. raz pobieramy bazę itp.
+
+   // private static final Singleton INSTANCE = new Singleton(); //gdy ta linijka pozostanie w linii 5 to ta instan cja bedzie się tutaj najpierw tworzyła
+                                                               //dlatego możemy przenieść to do metody i pozostawić tylko inicjalizację tego pola jak poniżej
+
+    private static Singleton INSTANCE;
+
+    private Singleton() {
+        System.out.println("Instancja - mimo wywołania metody 4x w main to tylko raz się uruchamia");
+    }
+
+    public static Singleton getInstance() {
+       //tutaj tworzymy instację w metodzie
+        if (INSTANCE == null){
+            INSTANCE = new Singleton();
+        }
+
+        return INSTANCE;
+    }
+
+    public static void main(String[] args) {
+
+        getInstance();
+        getInstance();
+        getInstance();
+        getInstance();
+
+    }
+}
