d0447d6fd2d4c01cead9431e7ae16c5e29e2279b
ksx <kd.stajkowski@gmail.com>
2022-08-02 11:26:29 +0200

    Fabryka

---
+++ src/main/java/org/example/wzorceProjektowe/MainFactory.java
@@ -0,0 +1,34 @@
+package org.example.wzorceProjektowe;
+
+public class MainFactory { // method that creates objects
+
+    public static void main(String[] args) {
+      //  new Car(); //instancja obiektu - zamiast tego użyjemy metody createCar jako wzorzec Fabryka czyli metode która tworzy obiekty
+        createCar(); //tworzę n obiektów dzięki fabryce
+        createCar();
+        createCar();
+        createColorCar("Zielony"); //w zależności od konstruktorów mogę tworzyć różne warianty
+        createColorCar("Niebieski");
+    }
+
+    //factory pattern - jest to metoda statycna dzięki której nie musimy tworzyć instancji klasy aby skorzystac z metody
+    public static Car createCar(){
+        return new Car();
+    }
+
+    public static Car createColorCar(String color){
+        return new Car(color);
+    }
+    public static class Car { //instancja klasy
+        Car() {//konstruktor
+            System.out.println("Car created");
+        }
+
+        Car(String color) {//konstruktor
+            System.out.println("Color car created - " + color);
+        }
+
+    }
+
+
+}
