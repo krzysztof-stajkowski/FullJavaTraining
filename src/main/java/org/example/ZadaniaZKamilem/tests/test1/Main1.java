64424ad1c6c340b5fa0c76f6e06b05a897cdc2b1
ksx <kd.stajkowski@gmail.com>
2022-08-11 19:56:51 +0200

    tests

--- src/main/java/org/example/ZadaniaZKamilem/tests/test1/Main1.java
+++ src/main/java/org/example/ZadaniaZKamilem/tests/test1/Main1.java
@@ -2,11 +2,8 @@ package org.example.ZadaniaZKamilem.tests.test1;
 
 public class Main1 {
     public static void main(String[] args) {
-
-
-
         One one = new One();
-        Two two = (Two)one; //Exception in thread "main" java.lang.ClassCastException:
+        Two two = (Two)one;
         System.out.println( two.getInt() );
     }
 }
