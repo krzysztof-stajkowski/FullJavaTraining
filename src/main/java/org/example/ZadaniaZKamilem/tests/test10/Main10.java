fe3bc7bf7ff2d16e0e161b955528dd539939d69d
ksx <kd.stajkowski@gmail.com>
2022-08-11 20:47:49 +0200

    tests

---
+++ src/main/java/org/example/ZadaniaZKamilem/tests/test10/Main10.java
@@ -0,0 +1,9 @@
+package org.example.ZadaniaZKamilem.tests.test10;
+
+public class Main10 {
+    public static void main(String[] args) {
+        char ch = 'c'; //tu był "" i chciało String i zmieniłem na single quote aby był Char i zmieniłem typ
+        One one = new One(ch); // tu był błąd ponieważ /jak wyżej
+
+    }
+}
---
+++ src/main/java/org/example/ZadaniaZKamilem/tests/test10/One.java
@@ -0,0 +1,15 @@
+package org.example.ZadaniaZKamilem.tests.test10;
+
+public class One {
+    public One(int x){
+        System.out.print("int constructor");
+    }
+
+    public One(char c){
+        System.out.print("char constructor");
+    }
+
+    void One(String str){
+        System.out.print("String constructor");
+    }
+}
